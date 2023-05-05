1a84ccaf3f5d721738af47611be47b48da5a9694
sangam-pawar <sangam6119@gmail.com>
2023-05-05 16:09:31 +0530

    jj

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+
+app.get("/", (req,res) => {
+    console.log('Here')
+    res.render("Home", {text : 'World'} )
+    
+})
+
+app.get('/Dashboard', (  req , res) =>{
+  res.render("Dashboard")
+} )
+
+
+// app.post('/Dashboard', (  req , res) =>{
+//     console.log(req.body);
+//   } )
+
+app.post('/Dashboard' , ( req,res) => {
+    console.log(req.body);
+    res.render("Order" , {Info:req.body})
+})
+app.get('/Order',(req,res)  =>{
+    res.render("Order")
+})
+
+app.listen(3000)
