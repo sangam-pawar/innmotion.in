fdcf5db8e52b3024781d2507ed8cfa478d051878
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-05-06 14:16:33 +0530

    logo

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
+app.use(express.static("public"));
+
 
 app.get("/", (req,res) => {
     console.log('Here')
